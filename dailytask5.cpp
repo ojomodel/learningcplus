671cdfaae39db55d06923d3b8c33b2e712c2b013
ojomodel <seunomodelee@gmail.com>
2026-01-07 16:50:58 -0600

    move to windows pc

---
+++ dailytask5.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+enum Job { Warrior = 1 , Mage = 2, Rogue = 3};
+struct Stats {
+    string strength;
+    string intelligence;
+ };
+
+
+
+int main()
+{
+    
+    return 0;
+}
